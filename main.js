55a31d46d88f598890206d965b5fb74fdab5c29e
JonathanNoH <jonathan897h@gmail.com>
2022-02-25 12:42:22 -0700

    make displayBooks function with console

--- main.js
+++ main.js
@@ -3,7 +3,22 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    this.info = function() {
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead ? "read" : "not read yet"}`;
+}
+
+Book.prototype.info = function() {
+return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead ? "read" : "not read yet"}`;
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    let newBook = new Book(title, author, pages, isRead)
+    myLibrary.push(newBook);
+}
+
+let myLibrary = [];
+
+function displayBooks(lob) {
+    if (lob.length === 0) return;
+    for (let book of lob) {
+        console.log(book);
     }
-  }
+}
