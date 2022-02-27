a470ab454c504617ec08bd867f00a442e45a4e23
JonathanNoH <jonathan897h@gmail.com>
2022-02-27 11:23:21 -0700

    add removeBook function and data attributes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* BOOK OBJECT CONSTRUCTOR */
+
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
@@ -9,24 +11,27 @@ Book.prototype.info = function() {
 return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead ? "read" : "not read yet"}`;
 }
 
+/* FUNCTIONS */
+
 function addBookToLibrary(title, author, pages, isRead) {
     let newBook = new Book(title, author, pages, isRead)
     myLibrary.push(newBook);
 }
 
-let myLibrary = [];
-
 function displayBooks() {
     if (myLibrary.length === 0) return;
+    let bookNum = 0;
     for (let book of myLibrary) {
         console.log(book);
-        displayOneBook(book);
+        displayBook(book, bookNum);
+        bookNum += 1;
     }
 }
 
-function displayBook(book) {
+function displayBook(book, bookNum) {
     let card = document.createElement("div");
-    card.classList.add('card')
+    card.classList.add('card');
+    card.setAttribute('data-book-num', bookNum);   
     for (let property in book) {
         if (property == 'info') break;
         let div = document.createElement("div");
@@ -44,26 +49,45 @@ function displayBook(book) {
     main.appendChild(card);
 }
 
-let main = document.querySelector("main");
-let showFormButton = document.querySelector(".show-form");
+function removeBook(bookNum) {
+    myLibrary.splice(bookNum, 1);
+    let littleOne = document.querySelector(`[data-book-num='${bookNum}'`);
+    /* I'm sorry littleOne */
+    main.removeChild(littleOne);
+}
 
-showFormButton.addEventListener('click', toggleForm);
 
 function toggleForm() {
     form.classList.toggle('reveal');
 }
 
+function clearMain() {
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+}
 
+/* VARIABLES */
+
+const main = document.querySelector("main");
+const showFormButton = document.querySelector(".show-form");
 const form = document.getElementById('add-book-form');
 const title = form.elements['book-title'];
 const author = form.elements['book-author'];
 const pages = form.elements['book-pages'];
 const read = form.elements['book-read'];
 
+let myLibrary = [];
+
+/* EVENT LISTENERS */
+
+showFormButton.addEventListener('click', toggleForm);
+
 form.addEventListener('reset', (e) => {
     let isRead = read.checked ? true : false;
     addBookToLibrary(title.value, author.value, pages.value, isRead);
-    displayBook(myLibrary[myLibrary.length - 1]);
+    clearMain();
+    displayBooks();
     toggleForm();
 });
 
