b7e4a18261a8418d4c777ecfc36c84ba1eba5729
JonathanNoH <jonathan897h@gmail.com>
2022-03-23 12:02:06 -0600

    use eslint to fix some style

--- main.js
+++ main.js
@@ -1,119 +1,125 @@
+/* eslint-disable no-continue */
+/* eslint-disable no-restricted-syntax */
 /* BOOK OBJECT CONSTRUCTOR */
 
 class Book {
-    constructor(title, author, pages, isRead) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.isRead = isRead;
-    }
-    toggle() {
-        this.isRead = this.isRead ? false : true;
-    }
-}
+  constructor(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
 
-/* FUNCTIONS */
-
-function addBookToLibrary(title, author, pages, isRead) {
-    let newBook = new Book(title, author, pages, isRead)
-    myLibrary.push(newBook);
-}
-
-function displayBooks() {
-    clearMain();
-    if (myLibrary.length === 0) return;
-    let bookNum = 0;
-    for (let book of myLibrary) {
-        displayBook(book, bookNum);
-        bookNum += 1;
-    }
-}
-
-function displayBook(book, bookNum) {
-    /* create card */
-    let card = document.createElement("div");
-    card.classList.add('card');
-
-    /* set up remove button */
-    card.setAttribute('data-book-num', bookNum);
-    let removeButton = document.createElement("button");
-    let buttonNode = document.createTextNode("X");
-    removeButton.appendChild(buttonNode);
-    removeButton.classList.add('remove-button');
-    removeButton.addEventListener('click', (e) => {
-        removeBook(bookNum);
-    })
-
-    /* set up read button */
-
-    let readButton = document.createElement('button');
-    let readButtonNode = document.createTextNode("change");
-    readButton.appendChild(readButtonNode);
-    readButton.classList.add('read-button');
-    readButton.addEventListener('click', (e) => {
-        toggleRead(bookNum);
-    });
-
-    /* set up book properties */
-    for (let property in book) {
-        if (property == 'toggle') break;
-        let div = document.createElement("div");
-        div.classList.add(`${property}`);
-        let node;
-        if (property == `isRead`) {
-            const str = book[property] ? "Read" : "Not Read Yet";
-            node = document.createTextNode(str);
-        } else {
-            node = document.createTextNode(book[property]);
-        }
-        div.appendChild(node);
-        card.appendChild(div);
-    }
-    card.appendChild(readButton);
-    card.appendChild(removeButton);
-    main.appendChild(card);
-}
-
-function removeBook(bookNum) {
-    myLibrary.splice(bookNum, 1);
-    displayBooks();
-}
-
-function toggleRead(bookNum) {
-    myLibrary[bookNum].toggle();
-    displayBooks();
-}
-
-function toggleForm() {
-    form.classList.toggle('reveal');
-}
-
-function clearMain() {
-    while (main.firstChild) {
-        main.removeChild(main.firstChild);
-    }
+  toggle() {
+    this.isRead = !this.isRead;
+  }
 }
 
 /* VARIABLES */
 
-const main = document.querySelector("main");
-const showFormButton = document.querySelector(".show-form");
+const main = document.querySelector('main');
+const showFormButton = document.querySelector('.show-form');
 const form = document.getElementById('add-book-form');
-const title = form.elements['book-title'];
-const author = form.elements['book-author'];
-const pages = form.elements['book-pages'];
-const read = form.elements['book-read'];
+const titleDOM = form.elements['book-title'];
+const authorDOM = form.elements['book-author'];
+const pagesDOM = form.elements['book-pages'];
+const readDOM = form.elements['book-read'];
 
-let myLibrary = [];
+const myLibrary = [];
+
+/* FUNCTIONS */
+
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+}
+
+function toggleForm() {
+  form.classList.toggle('reveal');
+}
+
+function clearMain() {
+  while (main.firstChild) {
+    main.removeChild(main.firstChild);
+  }
+}
+
+function displayBook(book, bookNum) {
+  /* create card */
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  /* set up remove button */
+  card.setAttribute('data-book-num', bookNum);
+  const removeButton = document.createElement('button');
+  const buttonNode = document.createTextNode('X');
+  removeButton.appendChild(buttonNode);
+  removeButton.classList.add('remove-button');
+  removeButton.addEventListener('click', () => {
+    removeBook(bookNum);
+    displayBooks();
+  });
+
+  /* set up read button */
+
+  const readButton = document.createElement('button');
+  const readButtonNode = document.createTextNode('change');
+  readButton.appendChild(readButtonNode);
+  readButton.classList.add('read-button');
+  readButton.addEventListener('click', () => {
+    toggleRead(bookNum);
+    displayBooks();
+  });
+
+  /* set up book properties */
+  for (const property in book) {
+    if (Object.prototype.hasOwnProperty.call(book, property)) {
+      if (property === 'toggle') continue;
+      const div = document.createElement('div');
+      div.classList.add(`${property}`);
+      let node;
+      if (property === 'isRead') {
+        const str = book[property] ? 'Read' : 'Not Read Yet';
+        node = document.createTextNode(str);
+      } else {
+        node = document.createTextNode(book[property]);
+      }
+      div.appendChild(node);
+      card.appendChild(div);
+    }
+  }
+  card.appendChild(readButton);
+  card.appendChild(removeButton);
+  main.appendChild(card);
+}
+
+function displayBooks() {
+  clearMain();
+  if (myLibrary.length === 0) return;
+  let bookNum = 0;
+  myLibrary.forEach((book) => {
+    displayBook(book, bookNum);
+    bookNum += 1;
+  });
+}
+
+function removeBook(bookNum) {
+  myLibrary.splice(bookNum, 1);
+  displayBooks();
+}
+
+function toggleRead(bookNum) {
+  myLibrary[bookNum].toggle();
+  displayBooks();
+}
 
 /* EVENT LISTENERS */
 
 showFormButton.addEventListener('click', toggleForm);
 
-form.addEventListener('reset', (e) => {
-    let isRead = read.checked ? true : false;
-    addBookToLibrary(title.value, author.value, pages.value, isRead);
-    displayBooks();
-    toggleForm();
+form.addEventListener('reset', () => {
+  const isRead = !!readDOM.checked;
+  addBookToLibrary(titleDOM.value, authorDOM.value, pagesDOM.value, isRead);
+  displayBooks();
+  toggleForm();
 });
-
